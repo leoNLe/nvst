0d2f55b491d00aac66872394dc2261a330743f3b
Leo Le <lnle125@gmail.com>
2020-06-09 19:30:18 -0700

    correction for eslint

--- models/transaction.js
+++ models/transaction.js
@@ -2,37 +2,37 @@ module.exports = (Sequelize, DataTypes) => {
   const Transactions = Sequelize.define("Transactions", {
     purchasePrice: {
       type: DataTypes.DECIMAL,
-      allowNull: false,
+      allowNull: false
     },
     stockSymbol: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [1, 4 ]
-      },
+      }
     },
     soldPrice: {
       type: DataTypes.DECIMAL,
-      default: null,
+      default: null
     },
     quality: {
       type: DataTypes.INTEGER,
-      allowNull: false,
-    },
+      allowNull: false
+    }
   });
   Transactions.associate = models => {
     Transactions.belongsTo(models.Users, {
       foreignKey: {
         name: "user_id",
-        allowNull: false,
-      },
+        allowNull: false
+      }
     });
 
     Transactions.belongsTo(models.Stocks, {
       foreignKey: {
         name: "stock_id",
-        allowNull: false,
-      },
+        allowNull: false
+      }
     });
   };
   return Transactions;
