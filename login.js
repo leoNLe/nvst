8dba6672fd7d60b7ce0930d028588756308e62f6
Cole Jackson <cole_jackson@live.com>
2020-06-11 22:22:26 -0700

    now on handlebars, now on sequelize

--- login.js
+++ login.js
@@ -1,45 +1,137 @@
-const express = require("express"),
-  routes = require("./routes"),
-  user = require("./routes/user"),
-  http = require("http"),
-  path = require("path");
-
-const session = require("express-session");
-const app = express();
-const mysql = require("mysql");
+const express = require("express");
 const bodyParser = require("body-parser");
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "mixVok-quxsy0-sinmup",
-  database: "stocks"
-});
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const User = require("./models/user");
+const hbs = require("express-handlebars");
+const path = require("path");
 
-connection.connect();
-global.db = connection;
-// env vars
-app.set("port", process.env.PORT || 8080);
-app.set("views", __dirname + "/views");
-app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
+const app = express();
+
+app.set("port", 8080);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
 app.use(
   session({
-    secret: "mixVok-quxsy0-sinmup",
+    key: "user_id",
+    secret: "password",
     resave: false,
-    saveUninitliazed: true,
-    cookie: { maxAge: 60000 }
+    saveUninitialized: false,
+    cookie: {
+      expires: 600000
+    }
   })
 );
-// login route
-app.get("/", routes.index);
-app.get("/signup", user.signup);
-app.post("/signup", user.signup);
-app.get("/login", routes.index);
-app.post("/login", user.login);
-app.get("/home/dashboard", user.dashboard);
-app.get("/home/logout", user.logout);
-app.get("home/profile", user.profile);
 
-app.listen(8080);
+app.engine(
+  "hbs",
+  hbs({
+    extname: "hbs",
+    defaultLayout: "layout",
+    layoutsDir: __dirname + "/views/layouts"
+  })
+);
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+app.use((req, res, next) => {
+  if (req.cookies.user_id && !req.session.user) {
+    res.clearCookie("user_id");
+  }
+  next();
+});
+
+//handle bars
+const hbsContent = {
+  userName: "",
+  loggedin: false,
+  title: "Not Logged In",
+  body: "placeholder"
+};
+
+// check for if user is already logged in
+const sessionChecker = (req, res, next) => {
+  if (req.session.user && req.cookies.user_id) {
+    res.redirect("/account");
+  } else {
+    next();
+  }
+};
+
+//home
+app.get("/", sessionChecker, (req, res) => {
+  res.redirect("/login");
+});
+
+// signup
+app
+  .route("/signup")
+  .get((req, res) => {
+    res.render("signup", hbsContent);
+  })
+  .post((req, res) => {
+    User.create({
+      username: req.body.username,
+      password: req.body.password
+    })
+      .then(user => {
+        req.session.user = user.dataValues;
+        res.redirect("/account");
+      })
+      .catch(error => {
+        res.redirect("/signup");
+      });
+  });
+
+//login
+app
+  .route("/login")
+  .get(sessionChecker, (req, res) => {
+    res.render("login", hbsContent);
+  })
+  .post((req, res) => {
+    const username = req.body.username,
+      password = req.body.password;
+
+    User.findOne({ where: { username: username } }).then(user => {
+      if (!user) {
+        res.redirect("/login");
+      } else if (!user.validPassword(password)) {
+        res.redirect("/login");
+      } else {
+        req.session.user = user.dataValues;
+        res.redirect("/account");
+      }
+    });
+  });
+
+// account
+app.get("/account", (req, res) => {
+  if (req.session.user && req.cookies.user_id) {
+    hbsContent.loggedin = true;
+    hbsContent.userName = req.session.user.username;
+    console.log(req.session.user.username);
+    hbsContent.title = "You are logged in";
+    res.render("index", hbsContent);
+  } else {
+    res.redirect("/login");
+  }
+});
+
+// logout
+app.get("/logout", (req, res) => {
+  if (req.session.user && req.cookies.user_id) {
+    hbsContent.loggedin = false;
+    hbsContent.title = "You are logged out!";
+    res.clearCookie("user_id");
+    console.log(JSON.stringify(hbsContent));
+    res.redirect("/");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.listen(app.get("port"), () => console.log(`App started on port ${app.get("port")}`)
+);
--- routes/user.js
+++ routes/user.js
@@ -1,115 +1,56 @@
-//signup
+const Sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
 
-exports.signup = function(req, res) {
-  message = "";
-  if (req.method == "POST") {
-    let post = req.body;
-    let name = post.user_name;
-    let pass = post.password;
-    let fname = post.first_name;
-    let lname = post.last_name;
-    let mob = post.mob_no;
+const sequelize = new Sequelize("stocks", "root", "mixVok-quxsy0-sinmup", {
+  host: "localhost",
+  port: 3306,
+  dialect: "mysql2",
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  operatorsAliases: false
+});
 
-    let sql =
-      "INSERT INTO `users`(`first_name, `last_name`, `mob_no`, `user_name`, `password`) VALUES ('" +
-      fname +
-      "','" +
-      lname +
-      "','" +
-      mob +
-      "','" +
-      name +
-      "','" +
-      pass +
-      "')";
-    let query = db.query(sql, function(err, result) {
-      message = "Success, Welcome To nvst!";
-      res.render("signup.ejs", { message: message });
-    });
-  } else {
-    res.render("signup");
+const Users = sequelize.define("users", {
+  id: {
+    type: Sequelize.INTEGER,
+    unique: true,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false
   }
+});
+
+Users.beforeCreate((user, options) => {
+  const cats = bcrypt.genCatsSync();
+  user.password = bcrypt.hashSync(user.password, cats);
+});
+
+Users.prototype.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
 };
 
-//login
+sequelize
+  .sync()
+  .then(() => console.log(
+      "users table has been successfully created, if one doesn't exist"
+    )
+  )
+  .catch(error => console.log("This error occured", error));
 
-exports.login = function(req, res) {
-  let message = "";
-  let sess = req.session;
-
-  if (req.method == "POST") {
-    let post = req.body;
-    let name = post.user_name;
-    let pass = post.password;
-
-    let sql =
-      "SELECT id, first_name, last_name, user_name FROM `users` WHERE `user_name` = '" +
-      name +
-      "' and password = '" +
-      pass +
-      "'";
-    db.query(sql, function(err, results) {
-      if (results.length) {
-        req.session.userID = results[0].id;
-        req.session.user = results[0];
-        console.log(results[0].id);
-        res.redirect("/home/dashboard");
-      } else {
-        message = "INVALID ID";
-        res.render("index.ejs", { message: message });
-      }
-    });
-  } else {
-    res.render("index.ejs", { message: message });
-  }
-};
-
-// Page functionality
-
-exports.dashboard = function(req, res, next) {
-  let user = req.session.user,
-    userID = req.session.userID;
-  console.log("ddd=" + userID);
-  if (userID == null) {
-    res.redirect("/login");
-    return;
-  }
-  let sql = " SELECT * FROM `users` WHERE = `id` = '" + userID + "'";
-  db.query(sql, function(err, results) {
-    res.render("dashboard.ejs", { user: user });
-  });
-};
-
-// LOGOUT
-
-exports.logout = function(req, res) {
-  req.session.destroy(function(err) {
-    res.redirect("/login");
-  });
-};
-
-// pull user details
-exports.profile = function(req, res) {
-  let userID = req.session.userID;
-  if (userID == null) {
-    res.redirect("/login");
-    return;
-  }
-  let sql = " SELECT * FROM `users` WHERE `id` = '" + userID + "'";
-  db.query(sql, function(err, result) {
-    res.render("profile.ejs", { data: result });
-  });
-};
-
-// edit user details
-exports.editProfile = function(req, res) {
-  var userID = req.session.userID;
-  if (userID == null) {
-    res.redirect("/login");
-    return;
-  }
-  var sql = "SELECT * FROM `users` WHERE `id` = '" + userID + "'";
-  db.query(sql, function(err, results) {
-    res.render("edit_profile.ejs", { data: results });
-  });
-};
+module.exports = User;
