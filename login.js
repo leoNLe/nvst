c3b8786597403a1e61690fa7a548ef7adf11da00
Leo Le <lnle125@gmail.com>
2020-06-16 19:46:33 -0700

    minor fixes and added db for heroku

--- login.js
+++ login.js
@@ -1,32 +1,12 @@
 const db = require("./models");
 const passport = require("./config/passport");
+const { authenticate } = require("./config/passport");
+const isAuthenticated = require("./config/middleware/isAuthenticated");
 module.exports = function(app) {
-  //handle bars
-  // const hbsContent = {
-  //   email: "",
-  //   loggedin: false,
-  //   title: "Not Logged In",
-  //   body: "placeholder"
-  // };
-
-  // check for if user is already logged in
-  // const sessionChecker = (req, res, next) => {
-  //   console.log("session checker:");
-  //   console.log(`req: ${req}`);
-  //   console.log(`res: ${res}`);
-  //   console.log(`next: ${next}`);
-  //   if (req.session.user && req.cookies.userId) {
-  //     res.redirect("/account");
-  //   } else {
-  //     next();
-  //   }
-  // };
-
   //home
   app.get("/", (req, res) => {
     res.redirect("/login.html");
   });
-
   // signup
   app.route("/signup").post((req, res) => {
     console.log("/signup");
@@ -48,48 +28,24 @@ module.exports = function(app) {
 
   //login
   app.post("/login", passport.authenticate("local"), (req, res) => {
-    // console.log("/login");
-    // console.log(req.user);
     res.json({ userId: req.user.dataValues.id });
-    // const email = req.body.email,
-    //   password = req.body.password;
-
-    // db.Users.findOne({ where: { email: email } }).then(user => {
-    //   console.log(user);
-    //   if (!user) {
-    //     res.redirect("/login");
-    //   } else if (!user.validPassword(password)) {
-    //     res.redirect("/login");
-    //   } else {
-    //     req.session.user = user.dataValues;
-    //     res.redirect("/portfolio");
-    //   }
-    // });
   });
-  // account
-  // app.get("/account", (req, res) => {
-  //   console.log("/account");
-  //   if (req.session.users && req.cookies.user_id) {
-  //     hbsContent.loggedin = true;
-  //     hbsContent.email = req.session.user.email;
-  //     console.log(req.session.user.email);
-  //     hbsContent.title = "Already Logged in, not you?";
-  //     res.render("index", hbsContent);
-  //   } else {
-  //     res.redirect("/login");
-  //   }
-  // });
 
-  // logout
-  // app.get("/logout", (req, res) => {
-  //   if (req.session.user && req.cookies.user_id) {
-  //     hbsContent.loggedin = false;
-  //     hbsContent.title = "Logged out, see you tomorrow!";
-  //     res.clearCookie("user_id");
-  //     console.log(JSON.stringify(hbsContent));
-  //     res.redirect("/");
-  //   } else {
-  //     res.redirect("/login.html");
-  //   }
-  // });
+  app.put("/update", isAuthenticated, async (req, res) => {
+    const userId = req.user.id;
+    const { firstName, lastName, email, password } = req.body;
+    console.log(req.user.id);
+    try {
+      const user = await db.Users.findOne({
+        where: { userId }
+      });
+
+      user.dataValues = { lastName, firstName, email, password };
+      user.save();
+      res.json({ success: true });
+    } catch (err) {
+      console.log(err);
+      res.status(501).send();
+    }
+  });
 };
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -9,106 +9,21 @@ const api = new finnhub.DefaultApi();
 */
 
 module.exports = function(app) {
-  // Using the passport.authenticate middleware with our local strategy.
-  // If the user has valid login credentials, send them to the members page.
-  // Otherwise the user will be sent an error
-  /*app.post("/api/login", passport.authenticate("local"), (req, res) => {
-    // Sending back a password, even a hashed password, isn't a good idea
-    res.json({
-      email: req.user.email,
-      id: req.user.id
-    });
-  });*/
-
-  // Company News
-  /* api.companyNews("AAPL", "2020-01-01", "2020-05-01", (error, data, response) => {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(util.inspect(data, false, null, true))
-    }
-});*/
-
-  /*const quotes = {
-  
-}
-
-  api.quote("AAPL", "D", quotes, (err, data, response) => {
-    if (error) {
-      console.log(error);
-  } else {
-      console.log(util.inspect(data, false, true, null));
-    }
-  }); */
-
-  /* api.stockSymbols("APPLE", "DISNEY", (error, data, response) => {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log(util.inspect(data, false, null, true));
-    }
-});
-
-  // Stock Candles
-  const stockCandlesOpts = {
-    from: 1572651390,
-    to: 1575243390
-  };
-  api.stockCandles("AAPL", "D", stockCandlesOpts, (error, data, response) => {
-    if (error) {
-      console.error(error);
-    } else {
-      console.log(util.inspect(data, false, null, true));
-    }
-  });*/
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
-
-  // const WebSocket = require("ws"); //npm i ws
-  // const socket = new WebSocket(
-  //   "wss://ws.finnhub.io?token=brcsl7nrh5rfdvppg6q0"
-  // ); //change token to personal token
-
-  // // Connection opened -> Subscribe
-  // socket.addEventListener("open", () => {
-  //   //for testing purposes
-  //   socket.send(JSON.stringify({ type: "subscribe", symbol: "AAPL" }));
-  //   socket.send(JSON.stringify({ type: "subscribe", symbol: "MSFT" }));
-  //   socket.send(JSON.stringify({ type: "subscribe", symbol: "AMZN" }));
-  //   socket.send(JSON.stringify({ type: "subscribe", symbol: "CRON" }));
-  //   socket.send(JSON.stringify({ type: "subscribe", symbol: "CGC" }));
+  // app.post("/api/signup", (req, res) => {
+  //   db.Users.create({
+  //     email: req.body.email,
+  //     firstName: req.body.firstName,
+  //     lastName: req.body.lastName,
+  //     password: req.body.password
+  //   })
+  //     .then(() => {
+  //       res.redirect(307, "/api/login");
+  //     })
+  //     .catch(err => {
+  //       console.log(err);
+  //       res.status(401).json(err);
+  //     });
   // });
-
-  // // Listen for messages
-  // socket.addEventListener("message", event => {
-  //   // prints to console
-  //   console.log("Message from server ", event.data);
-  // });
-
-  // Unsubscribe
-  /*const unsubscribe = function(symbol) {
-    // functionality to "sell" needs to be expanded upon currently would just remove from feed
-    socket.send(JSON.stringify({ type: "unsubscribe", symbol: symbol }));
-  };
-  */
-
-  app.post("/api/signup", (req, res) => {
-    db.Users.create({
-      email: req.body.email,
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      password: req.body.password
-    })
-      .then(() => {
-        console.log("here");
-        res.redirect(307, "/api/login");
-      })
-      .catch(err => {
-        console.log(err);
-        res.status(401).json(err);
-      });
-  });
 
   // Route for logging user out
   app.get("/logout", (req, res) => {
